cace32dbbaefea3ba60936c057db79d5fa7d6247
Eduardo Bolson Rauber <eduardo_farm@hotmail.com>
2023-10-04 16:36:40 -0300

    new classes and first functions

---
+++ Calculadora/Funcoes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Funcoes
+    {
+        public double Somar()
+        {
+            Console.Write("Digite o primeiro número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            return Math.Round(n1 + n2, 2);
+        }
+        public double Subtrair()
+        {
+            Console.Write("Digite o primeiro número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            return Math.Round(n1 - n2, 2);
+        }
+        public double Multiplicar()
+        {
+            Console.Write("Digite o primeiro número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            return Math.Round(n1 * n2, 2);
+        }
+        public double Dividir()
+        {
+            Console.Write("Digite o primeiro número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            return Math.Round(n1 / n2, 2);
+        }
+    }
+}
---
+++ Calculadora/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Menu
+    {
+        public string MenuPrincipal()
+        {
+            Console.WriteLine("---- Menu Principal ----");
+            Console.WriteLine();
+            Console.WriteLine("1- Somar");
+            Console.WriteLine("2- Subtrair");
+            Console.WriteLine("3- Multiplicar");
+            Console.WriteLine("4- Dividir");
+            Console.WriteLine();
+            Console.WriteLine("0- Sair");
+
+            return Console.ReadLine();
+
+        }
+    }
+}
